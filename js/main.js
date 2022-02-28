44c617efc18f50b0a33e8ed73dde2267cdcd52a6
simone andreozzi <simoneandreozzi8@gmail.com>
2022-02-28 14:13:21 +0100

    esercizio-correzzione-parte-random

--- js/main.js
+++ js/main.js
@@ -1,26 +1,57 @@
-// A) Chiedere utente la sua email
- const inserisciEmail = prompt("inserisci la tua email");
-/*  console.log(inserisciEmail);   */
+// A) Chiedere utente la sua emai
+     const inserisciEmail = prompt("inserisci la tua email").trim(); 
 // B) creare una lista di email
- const nomiEmail = ["marco.lombardi@libero.it","vanessa.brumotti@libero.com","giulia.bianchi@libero.it","antonio.lombardi@libero.it", "sara.rea@gmail.com"]
-/* console.log(nomiEmail);  */
-
-
+     const nomiEmail = ["marco.lombardi@libero.it","vanessa.brumotti@libero.com","giulia.bianchi@libero.it","antonio.lombardi@libero.it", "sara.rea@gmail.com"] 
+    console.log(nomiEmail);   
+    nomiEmail.push("francesco.grande@libero.it")
+    document.getElementById("newEmail").innerHTML =  inserisciEmail;
+     
+    
 //  c) creare un ciclo per controllare chi sia nella lista nomiEmail e se non è presente allora consolo.log(email non presente)
 
-    let contatore = 0;
+ let contatore = 0;
 
 for (let i = 0; i < nomiEmail.length; i++){
-   /*  console.log(nomiEmail[i]) */
+
     if( inserisciEmail == nomiEmail[i]){
         console.log("email corretta");
         contatore = contatore + 1;
     
     }
+    if ( contatore == 0){
+        console.log("email non presente");
+    }
+}   
+
+//  D) 
+   const player1 = Math.floor (Math.random() * 6) +1;
+   console.log ("il tuo risultato è" +player1)
+
+   const player2 = Math.floor (Math.random() * 6) +1;
+   console.log ("il mio risultato è" +player2)
     
-}  
-if ( contatore == 0){
-    console.log("email non presente");
-}
+    /* const Random = Math.floor(Math.random() *5) +1;
+    console.log(Random)   */
+    /* vado a gnerare un numero da 1 a 6 */
+
+    if (player1 > player2){
+        console.log("ho vinto io")
+    } else if (player2 >  player1){
+        console.log("ha vinto lui")
+    } else {
+        console.log("i numeri sono uguali")
+    }
+
+
+
+
+
+
+    //se il mio numero è maggiore del tuo ho vinto io
+    // altrimenti hai vinto tu
+    //se il numero è maggiore di 6 allora riscrivi di nuovo il numero
+
+
+
 
 
